b2a686f9d9b21d4c74389e16dc1dc03ce78d1e8a
bucong <bucong@sodo-tech.com>
2017-12-14 13:04:48 +0800

    vue

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../webapp'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
